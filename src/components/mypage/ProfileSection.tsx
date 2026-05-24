7d77f88d1897f7c49c124c665f25e072b9d722c7
김준희 <shadd3338@gmail.com>
2026-05-25 00:49:50 +0900

    #4 [FEAT] 마이페이지 프로필 섹션 컴포넌트 구현

---
+++ src/components/mypage/ProfileSection.tsx
@@ -0,0 +1,32 @@
+import { typo } from '../../styles/typography';
+
+interface ProfileSectionProps {
+  nickname: string;
+  onEditProfile?: () => void;
+}
+
+export default function ProfileSection({ nickname, onEditProfile }: ProfileSectionProps) {
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-baseline gap-[4px]">
+        <span className={`${typo.H1_Bd} text-[#191C1D] tracking-[-0.24px]`}>
+          {nickname}
+        </span>
+        <span className={`${typo.T1_Sb} text-[#3D4A3E]`}>
+          님
+        </span>
+      </div>
+
+      <div className="mt-[35px]">
+        <button
+          onClick={onEditProfile}
+          className="inline-flex flex-col items-center justify-center px-[24px] py-[8px] rounded-full border border-[#6D7B6D]"
+        >
+          <span className={`${typo.B2_Md} text-[#006D36] text-center`}>
+            프로필 수정
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
