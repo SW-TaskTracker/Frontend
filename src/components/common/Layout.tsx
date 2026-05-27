cbbfbd19468b8f532c975f0088958ca195ea9738
김준희 <shadd3338@gmail.com>
2026-05-27 17:43:25 +0900

    #6 [FEAT] 바텀바 구현

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import BottomNav from './BottomNav';
 
 interface LayoutProps {
   children: ReactNode;
@@ -7,7 +8,10 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="w-[390px] min-h-screen mx-auto bg-white overflow-x-clip">
-      {children}
+      <div className="pb-[64px]">
+        {children}
+      </div>
+      <BottomNav />
     </div>
   );
 }
